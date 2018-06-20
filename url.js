3851b565b3c869d56da8458768e0d35b7f7e0e3c
jordansm125 <40198764+jordansm125@users.noreply.github.com>
2018-06-20 19:29:38 +0800

    Add files via upload

---
+++ url.js
@@ -0,0 +1,6 @@
+function getPrefilledUrl() {
+	var prefilledUrl = "./src/form1pgWithImage.html";
+	return prefilledUrl;
+}
+
+document.getElementById("imageLocation").innerHTML = '<iframe id="surveyFrame" src="https://goo.gl/forms/gHPzIFzEFSN8CTjE2" width=1260 height="2250" frameborder="0" marginheight="0" marginwidth="150" style="background: #FFFFFF;">Loading...</iframe>';
